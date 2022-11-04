a532e32d4835f2aca328ddd5b3f5f9e37d86e176
Hemanth Pokala <114699692+Hemanth-110500@users.noreply.github.com>
2022-11-04 17:33:43 -0500

    Update airbnb_prediction.py
    updated .py file

--- airbnb_prediction.py
+++ airbnb_prediction.py
@@ -1,1 +1,22 @@
-
+import pandas as pd
+airbnb_listings = pd.read_csv("listings.csv")
+airbnb_listings.head(5)
+airbnb_listings.info()
+airbnb_listings.shape
+airbnb_reviews = pd.read_csv("reviews.csv")
+airbnb_reviews.head(5)
+airbnb_reviews.info()
+airbnb_reviews.shape
+airbnb_calendar = pd.read_csv("calendar.csv")
+airbnb_calendar.head(5)
+airbnb_calendar.info()
+airbnb_calendar.shape
+null = airbnb_listings.isna().sum()
+null.sort_values(ascending=False)
+airbnb_listings.columns
+null = airbnb_reviews.isna().sum()
+null.sort_values(ascending=False)
+airbnb_reviews.columns
+null = airbnb_calendar.isna().sum()
+null.sort_values(ascending=False)
+airbnb_calendar.columns
