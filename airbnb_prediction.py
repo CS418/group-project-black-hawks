1a830b17a9807ea492702d2d2834ab2ac014d359
Hemanth Pokala <114699692+Hemanth-110500@users.noreply.github.com>
2022-11-04 17:52:51 -0500

    Update airbnb_prediction.py

--- airbnb_prediction.py
+++ airbnb_prediction.py
@@ -3,14 +3,17 @@ airbnb_listings = pd.read_csv("listings.csv")
 airbnb_listings.head(5)
 airbnb_listings.info()
 airbnb_listings.shape
+airbnb_listings.describe()
 airbnb_reviews = pd.read_csv("reviews.csv")
 airbnb_reviews.head(5)
 airbnb_reviews.info()
 airbnb_reviews.shape
+airbnb_reviews.describe()
 airbnb_calendar = pd.read_csv("calendar.csv")
 airbnb_calendar.head(5)
 airbnb_calendar.info()
 airbnb_calendar.shape
+airbnb_calendar.describe()
 null = airbnb_listings.isna().sum()
 null.sort_values(ascending=False)
 airbnb_listings.columns
